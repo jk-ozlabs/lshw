jk-ozlabs/lshw

commit 72cdd63f182f5f1a86d8a8ffce5255a90da73522

device-tree: Add parsed firmware version info
Signed-off-by: Jack Miller <jack@codezen.org>

src/core/device-tree.cc
@@ -214,6 +214,36 @@ static void scan_devtree_bootrom(hwNode & core)
   }
 }
 
+static void scan_devtree_firmware_powernv(hwNode &core)
+{
+  struct dirent **namelist;
+  int n;
+
+  pushd(DEVICETREE "/ibm,firmware-versions");
+  n = scandir(".", &namelist, selectfile, alphasort);
+  popd();
+
+  if (n < 0)
+    return;
+
+  for (int i = 0; i < n; i++)
+  {
+    string sname = string(namelist[i]->d_name);
+    string fullpath = string(DEVICETREE) + "/ibm,firmware-versions/" + sname;
+
+    if (sname == "linux,phandle" || sname == "name" || sname == "phandle")
+        continue;
+
+    hwNode newnode("firmware");
+    newnode.setDescription(sname);
+    newnode.setVersion(get_string(fullpath));
+    newnode.claim();
+    core.addChild(newnode);
+
+    free(namelist[i]);
+  }
+  free(namelist);
+}
 
 static string cpubusinfo(int cpu)
 {
@@ -1191,6 +1221,7 @@ bool scan_device_tree(hwNode & n)
     {
       core->addHint("icon", string("board"));
       scan_devtree_root(*core);
+      scan_devtree_firmware_powernv(*core);
       scan_devtree_cpu_power(*core);
       scan_devtree_memory_powernv(*core);
       n.addCapability("powernv", "Non-virtualized");

commit 6587223ab797ab1f70cd0d461d93454861aa86fe

device-tree: Fix FW version info logic
Validate node existence before reading. Also fix memory leak.

Fixes: 72cdd63f
Signed-off-by: Vasant Hegde <hegdevasant@linux.vnet.ibm.com>

src/core/device-tree.cc
@@ -219,6 +219,9 @@ static void scan_devtree_firmware_powernv(hwNode &core)
   struct dirent **namelist;
   int n;
 
+  if (!exists(DEVICETREE "/ibm,firmware-versions"))
+      return;
+
   pushd(DEVICETREE "/ibm,firmware-versions");
   n = scandir(".", &namelist, selectfile, alphasort);
   popd();
@@ -231,8 +234,10 @@ static void scan_devtree_firmware_powernv(hwNode &core)
     string sname = string(namelist[i]->d_name);
     string fullpath = string(DEVICETREE) + "/ibm,firmware-versions/" + sname;
 
-    if (sname == "linux,phandle" || sname == "name" || sname == "phandle")
+    if (sname == "linux,phandle" || sname == "name" || sname == "phandle") {
+        free(namelist[i]);
         continue;
+    }
 
     hwNode newnode("firmware");
     newnode.setDescription(sname);